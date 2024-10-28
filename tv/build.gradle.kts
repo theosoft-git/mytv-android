495d1ca7af7745999e17775729762ea0a0599134
yaoxieyoulei <1622968661@qq.com>
2024-10-28 19:25:13 +0800

    :bookmark: tv3.3.2

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 2
-        versionName = "3.3.1"
+        versionName = "3.3.2"
         vectorDrawables {
             useSupportLibrary = true
         }
